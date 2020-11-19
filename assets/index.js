5dc1727cbe6e05b6da61dd33540d0879c20a3e47
Byron Jimenez <byronjimenez9911@gmail.com>
2020-11-18 20:08:02 -0600

    Service Workers

--- assets/index.js
+++ assets/index.js
@@ -13,3 +13,9 @@ const player = new MediaPlayer({el: video, plugins : [
 
 togglePlay.onclick = () => player.togglePlay()
 toggleMute.onclick = () => player.toggleMute()
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(error => {
+    console.log(error.message);
+  });
+}
